10f81b92a826e7fab0cb3837655b93c34988a37f
CerBeer <ocelopp@gmail.com>
2024-10-05 01:04:45 +0300

    feat: streams full implemented properly

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import path, { dirname } from "path";
+import fs from "fs";
+
+const args = process.argv;
+const __filename = args[1];
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = "files";
+  const fileName = "fileToRead.txt";
+  const fileFullName = path.join(__dirname, filePath, fileName);
+  const input = fs.createReadStream(fileFullName);
+  input.on("data", (chunk) => process.stdout.write(chunk.toString()));
+  input.on("end", () => process.stdout.write("\n"));
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import Transform, { pipeline } from "stream";
+import path, { dirname } from "path";
+import fs from "fs";
+
+const args = process.argv;
+const __filename = args[1];
+const __dirname = dirname(__filename);
+const filePath = "files";
+const fileName = "fileToRead.txt";
+const fileFullName = path.join(__dirname, filePath, fileName);
+const fileNameWrite = "fileToWrite.txt";
+const fileFullNameWrite = path.join(__dirname, filePath, fileNameWrite);
+
 const transform = async () => {
-    // Write your code here 
+  process.stdin.resume();
+  process.stdin.setEncoding("utf-8");
+
+  process.stdin.on("data", (inputStdin) => {
+    const result = inputStdin.replace('\n', '');
+    process.stdout.write(`${result.split("").reverse().join("")}\n\n`)
+  });
+  process.stdin.on("end", () => process.stdout.end());
 };
 
 await transform();
