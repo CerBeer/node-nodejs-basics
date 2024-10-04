0ffdcc06b3a7db109c7279663b65a712a70dac55
CerBeer <ocelopp@gmail.com>
2024-10-05 00:56:45 +0300

    feat: cli full implemented properly

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsRSS = new Array;  
+  const args = process.argv;
+  let prevIsKey = false;
+  let currArg = '';
+  args.forEach((val) => {
+    const currIsKey = val.startsWith('--');
+    if (!prevIsKey && currIsKey) {
+      currArg = val.slice(2);
+    } else if (prevIsKey && !currIsKey) {
+      argsRSS.push(`${currArg} is ${val}`);
+      currArg = '';
+    } else if (prevIsKey && currIsKey) {
+      argsRSS.push(`${currArg}`);
+      currArg = val.slice(2);
+    };
+    prevIsKey = currIsKey;
+  });
+  if (prevIsKey) argsRSS.push(`${currArg}`);
+  if (argsRSS.length) console.log(argsRSS.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envRSS = new Array;  
+  const env = process.env;
+  for (let key in env) {
+    if (key.startsWith('RSS_')) envRSS.push(`${key}=${env[key]}`);
+  }
+  if (envRSS.length) console.log(envRSS.join('; '));
 };
 
 parseEnv();
