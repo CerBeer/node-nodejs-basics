80970dd1bc3906ce6448c119f51cf2ffd3ec24aa
CerBeer <ocelopp@gmail.com>
2024-10-05 01:00:42 +0300

    feat: hash full implemented properly

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import path, { dirname } from "path";
+import crypto from "crypto";
+import fs from "fs";
+
+const args = process.argv;
+const __filename = args[1];
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = "files";
+  const fileName = "fileToCalculateHashFor.txt";
+  const fileFullName = path.join(__dirname, filePath, fileName);
+  const input = fs.createReadStream(fileFullName);
+
+  const fileHash = await new Promise((resolve, reject) => {
+    const hash = crypto.createHash("sha256");
+    const rs = input;
+    rs.on("error", reject);
+    rs.on("data", (chunk) => hash.update(chunk));
+    rs.on("end", () => resolve(hash.digest("hex")));
+  });
+
+  process.stdout.write(fileHash + "\n");
 };
 
 await calculateHash();
