1f2cf5232c5d697530f0519760951640f2438c61
CerBeer <ocelopp@gmail.com>
2024-10-04 23:43:54 +0300

    feat: rename.js implemented properly

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fs from "node:fs/promises";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const rename = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const filePath = "files";
+  const wrongFileName = "wrongFilename.txt";
+  const properFileName = "properFilename.md";
+
+  const wrongFileFullName = path.join(basePath, filePath, wrongFileName);
+  const properFileFullName = path.join(basePath, filePath, properFileName);
+  const errorMessage = "FS operation failed";
+
+  let errorNeedThrow = await fs
+    .stat(wrongFileFullName)
+    .then((stats) => !stats.isFile())
+    .catch(() => true);
+  errorNeedThrow =
+    errorNeedThrow ||
+    (await fs
+      .access(properFileFullName)
+      .then(() => true)
+      .catch(() => false));
+  if (errorNeedThrow) throw new Error(errorMessage);
+
+  await fs.rename(wrongFileFullName, properFileFullName);
 };
 
 await rename();
