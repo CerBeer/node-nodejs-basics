362323b835684d9dc174bbaf2f8ea90090db277a
CerBeer <ocelopp@gmail.com>
2024-10-04 23:24:20 +0300

    feat: create.js implemented properly

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "node:fs/promises";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const create = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const filePath = "files";
+  const fileName = "fresh.txt";
+  const fileFullNameToWrite = path.join(basePath, filePath, fileName);
+  const contentToWrite = "I am fresh and young";
+  const errorMessage = "FS operation failed";
+  const errorNeedThrow = await fs
+    .access(fileFullNameToWrite)
+    .then(() => true)
+    .catch(() => false);
+  if (errorNeedThrow) throw new Error(errorMessage);
+  await fs.writeFile(fileFullNameToWrite, contentToWrite);
 };
 
 await create();
