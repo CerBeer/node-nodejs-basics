42b27e82da00efd87f56ac115c6b882c470f1ad3
CerBeer <ocelopp@gmail.com>
2024-10-05 00:50:36 +0300

    feat: fs full implemented properly

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from "node:fs/promises";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const read = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const filePath = "files";
+  const fileName = "fileToRead.txt";
+  const fileFullNameToRead = path.join(basePath, filePath, fileName);
+  const errorMessage = "FS operation failed";
+  const errorNeedThrow = await fs
+    .stat(fileFullNameToRead)
+    .then((stats) => !stats.isFile())
+    .catch(() => true);
+  if (errorNeedThrow) throw new Error(errorMessage);
+  const contents = await fs.readFile(fileFullNameToRead, { encoding: "utf8" });
+  console.log(contents);
 };
 
 await read();
