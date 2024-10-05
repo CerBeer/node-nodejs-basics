3b2718d67d5517c8092a8c6e06538879d102bf05
CerBeer <ocelopp@gmail.com>
2024-10-05 17:58:06 +0300

    feat: Worker Threads full implemented properly

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from "child_process";
+import path from "path";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const file = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  fork(filePath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+
+spawnChildProcess(file, [1, 2, 3, "str"]);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import path from "path";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const performCalculationsNumber = async (incrementalNumber) => {
+    const cores = cpus().length;
+    const file = path.join(__dirname, "worker.js");
+  
+    const createWorkerThread = (num) =>
+      new Promise((resolve) => {
+        const worker = new Worker(file, { workerData: num });
+  
+        worker.on("message", (data) =>
+          resolve({
+            status: "resolved",
+            data,
+          })
+        );
+  
+        worker.on("error", (data) =>
+          resolve({
+            status: "error",
+            data: null,
+          })
+        );
+      });
+  
+    const workerThreads = Array.from({ length: cores }, (_, i) =>
+      createWorkerThread(incrementalNumber + i)
+    );
+    const results = await Promise.all(workerThreads);
+    console.log(results);
+  };
+
 const performCalculations = async () => {
-    // Write your code here
+    await performCalculationsNumber(10);
 };
 
 await performCalculations();
