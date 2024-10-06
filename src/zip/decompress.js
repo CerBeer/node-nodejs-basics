674c85051fa09e47553674b875a5ac91455f2ce9
CerBeer <ocelopp@gmail.com>
2024-10-06 15:39:38 +0300

    refactor: zip unzip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,21 +2,21 @@ import path, { dirname } from "path";
 import fs from "fs";
 import { createUnzip } from "zlib";
 
-const args = process.argv;
-const __filename = args[1];
-const __dirname = dirname(__filename);
-
-const filePath = "files";
-const fileName = "archive.gz";
-const fileZipName = "fileToCompress.txt";
-const fileFullName = path.join(__dirname, filePath, fileName);
-const fileZipFullName = path.join(__dirname, filePath, fileZipName);
-
-const gzip = createUnzip();
-const input = fs.createReadStream(fileFullName);
-const output = fs.createWriteStream(fileZipFullName);
-
 const decompress = async () => {
+  const args = process.argv;
+  const __filename = args[1];
+  const __dirname = dirname(__filename);
+  
+  const filePath = "files";
+  const fileName = "archive.gz";
+  const fileZipName = "fileToCompress.txt";
+  const fileFullName = path.join(__dirname, filePath, fileName);
+  const fileZipFullName = path.join(__dirname, filePath, fileZipName);
+  
+  const gzip = createUnzip();
+  const input = fs.createReadStream(fileFullName);
+  const output = fs.createWriteStream(fileZipFullName);
+
   input.pipe(gzip).pipe(output);
 };
 
